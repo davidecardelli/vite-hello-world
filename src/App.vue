30f61e19282104990345f55b871ddf1ae8583d3b
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-20 08:52:56 +0100

    Milestone 1 and Bonus

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script>
+import AppTitle from './components/AppTitle.vue';
+import AppJumbo from './components/AppJumbo.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        AppTitle,
+        AppJumbo,
+    },
+}
 
 </script>
 
 <template>
 
+    <div class="container">
+        <app-title></app-title>
+        <app-jumbo></app-jumbo>
+    </div>
+
 </template>
 
 <style>
-
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
 </style>
 
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<template>
+    <img src="../assets/jumbo-bg.png" alt="Jumbo">
+</template>
+
+<style>
+img {
+    width: 100%;
+    height: 100%;
+}
+</style>
+
